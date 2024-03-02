8e3e4d2d7968f6c3c1605ed71468670366c7abf4
pratikshinde1 <pratikshinde3095@gmail.com>
2024-03-02 16:00:18 +0530

    Furniture-Shop-Website Project

---
+++ script.js
@@ -0,0 +1,83 @@
+// let menus=document.getElementById("menu")
+
+function myFunction(){
+    var x = document.querySelector("#menu"); 
+     
+     if(x.style.display == "block")
+     {
+         x.style.display = "none";
+        //  x.style.display = "block";
+     }
+     else{
+        x.style.display = "block";
+     }
+ }
+
+ 
+// const toggle=()=>{
+//     if(menus.style.display == "none"||menus.style.display == " block" ||menus.style.textalign =="left"){
+//     //    menus.style.display = " block"
+//     //    menus.style.textalign ="left"
+//     }else if( menus.style.display="left"){
+        
+        
+//         menus.style.display="none"
+         
+//     }  
+//     else menus.style.display="flex"
+    
+
+// }
+   
+// let btn=document.querySelector("btn")
+
+// function sub(){  
+//     let n=document.getElementById("n1").value; 
+      
+//     if(n=='' || n==null){  
+//         alert("Enter each details correctly");  
+//         return false;
+//         }  
+//         else if(n!=Number)  
+//                 {  
+//                     alert('Name is incorrect must contain not a number  '); 
+//                     return false; 
+//                 }  
+
+//         else 
+//         {  
+//         alert("successfully... Redirecting to ");  
+//         return true;
+//                     window.location="https://www.wholesaleoffices.com/";  
+//                     }  
+
+// }
+
+// btn.onclick=function(){
+//     alert("Hello World!")
+// }
+ 
+
+// submit.onclick=function(){
+//     alert("btn clicked")
+//   }
+ 
+
+//  const mob=document.querySelector(".mobile-nav-btn")
+//  const hed=document.querySelector(".header")
+//  const toggleNavbar =()=>{
+//     hed.classList.toggle("active");
+//  };
+//  mob.addEventListener("click",()=>toggleNavbar())
+
+// function myFunction(){
+//     var x = document.getElementById("menu"); 
+     
+//      if(x.style.display == "block")
+//      {
+//          x.style.display = "none";
+//      }
+//      else{
+//          x.style.display = "block";
+//      }
+//  }
